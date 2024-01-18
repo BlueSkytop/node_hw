57762246131c4aacf2918aec97681cdb2cbe5d3b
BlueSkytop <olgakutnja@gmail.com>
2024-01-18 13:28:24 +0200

    add hw2

--- app.js
+++ app.js
@@ -1,65 +1,127 @@
-const fs = require('fs');
-const path = require('path');
+// HW – закінчити з CRUD операціями.
+//     Винести базу даних в json.file, при створенні записувати туда нових юзерів через fs
+// При створенні валідацію на імя і вік, імя повинно бути більше 3 символів, вік – не менше нуля
+// На гет, пут, деліт юзерів перевірити чи такий юзер є
 
+const express = require('express');
+const {read, write} = require('./fs.service');
 
-fs.mkdir('./text1', (err)=>{
-    fs.writeFile('./text1/text1.txt', 'Good morning', (err)=>{
-        if(err)throw new Error();
-});
-});
+const app = express();
 
-fs.mkdir('./text2', (err)=>{
-    fs.writeFile('./text2/text2.txt', 'Good day', (err)=>{
-        if(err)throw new Error();
-    });
-});
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-fs.mkdir('./text3', (err)=>{
-    fs.writeFile('./text3/text3.txt', 'Good evening', (err)=>{
-        if(err)throw new Error();
-    });
-});
-
-fs.mkdir('./text4', (err)=>{
-    fs.writeFile('./text4/text4.txt', 'Hello Okten', (err)=>{
-        if(err)throw new Error();
-    });
-});
-
-fs.mkdir('./text5', (err)=>{
-    fs.writeFile('./text5/text5.txt', 'How are you?', (err)=>{
-        if(err)throw new Error();
-    });
-});
-/*fs.readdirSync('.', {withFileTypes:true})
-    .forEach((dirTitle)=> {
-        if (dirTitle.isDirectory()) {
-            if (dirTitle.isFile()) {
-                console.log(`File: ${dirTitle.name}`);}
-            console.log(`Folder: ${dirTitle.name}`);
-        }
-    });*/
-const pathToCreatedFolder =path.join(__dirname)
-fs.readdir(pathToCreatedFolder,(err, files)=>{
-    for (const textElement of files) {
-        const filePath=path.join(pathToCreatedFolder, textElement);
-        const stats = fs.statSync(filePath);
-
-        if (stats.isFile()) {
-            console.log(`${textElement}: is a file`);
-        }else if (stats.isDirectory()) {
-            console.log(`${textElement}: is a folder`);
-
-            const innerFiles = fs.readdirSync(filePath);
-            for (const innerElement of innerFiles) {
-                const innerFilesPath = path.join(filePath, innerElement);
-                const innerStats = fs.statSync(innerFilesPath);
-                if (innerStats.isFile()) {
-                    console.log(`${innerElement}: is a file`);
-                }else  if (innerStats.isDirectory()) {
-                    console.log(`${innerElement}: is a folder`);
-                }
-            }
-        }
+app.get('/users', async (req, res) => {
+    try {
+        const users = await read();
+        res.status(200).json({data: users});
+    } catch (e) {
+        res.status(400).json(e.message);
     }
+});
+
+app.get('/users/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+
+        if (!Number.isInteger(id)) {
+            throw new Error('wrong ID param');
+        }
+
+        const users = await read();
+        const index = users.findIndex((user) => user.id === id);
+        if (index === -1) {
+            throw new Error('user not found');
+        }
+        res.json({data: users[index]});
+    } catch (e) {
+        res.status(400).json(e.message);
+    }
+});
+
+app.post('/users', async (req, res) => {
+    try {
+        const {email, name, age} = req.body;
+        if (!age || !Number.isInteger(age) || age <= 0 || age > 100) {
+            throw new Error('wrong age');
+        }
+        if (!email || !email.includes('@')) {
+            throw new Error('wrong email');
+        }
+        if (!name || name.length <= 3) {
+            throw new Error('wrong name');
+        }
+        const users = await read();
+
+        const newUser = {id: users[users.length - 1].id + 1, email, name, age};
+        users.push(newUser);
+        await write(users);
+
+        res.status(201).json({data: newUser});
+    } catch (e) {
+        res.status(400).json(e.message);
+    }
+});
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+
+        if (!Number.isInteger(id)) {
+            throw new Error('wrong ID param');
+        }
+
+        const users = await read();
+        const index = users.findIndex((user) => user.id === id);
+        if (index === -1) {
+            throw new Error('user not found');
+        }
+        users.splice(index, 1);
+        await write(users);
+
+        res.sendStatus(204);
+    } catch (e) {
+        res.status(400).json(e.message);
+    }
+});
+
+app.put('/users/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const {email, age, name} = req.body;
+
+        if (!Number.isInteger(id)) {
+            throw new Error('wrong ID param');
+        }
+
+        if (!age || !Number.isInteger(age) || age <= 0 || age > 100) {
+            throw new Error('wrong age');
+        }
+        if (!email || !email.includes('@')) {
+            throw new Error('wrong email');
+        }
+        if (!name || name.length <= 3) {
+            throw new Error('wrong name');
+        }
+
+        const users = await read();
+        const user = users.find((user) => user.id === id);
+        if (!user) {
+            throw new Error('user not found');
+        }
+        user.name = name;
+        user.age = age;
+        user.email = email;
+
+        await write(users);
+
+        res.status(201).json(user);
+    } catch (e) {
+        res.status(400).json(e.message);
+    }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server has started on PORT ${PORT}`);
 })
